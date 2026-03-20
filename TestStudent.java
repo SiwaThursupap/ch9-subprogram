ca7abad67ba8eeda0ecf6574bdfb819edf743983
SiwaThursupap <notsiwa1@gmail.com>
2026-03-20 15:13:52 +0700

    work from 20-3-69

---
+++ TestStudent.java
@@ -0,0 +1,11 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        student s1 = new student(68, "Siwa", 3.72);
+        student s2 = new student(69, "Manee", 2.5);
+        student s3 = new student(45, "Aalale", 1.0);
+        s1.displayInfo();
+        s2.displayInfo();
+        s3.displayInfo();
+        IO.println(s1.getInfo());
+    }
+}
---
+++ student.java
@@ -0,0 +1,23 @@
+public class student {
+
+    private int studentId;
+    private String studentName;
+    private double gpa;
+
+    public student(int studentId, String studentName, double gpa) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.gpa = gpa;
+    }
+
+    public void displayInfo() {
+        IO.println("--Student Info--");
+        IO.println("Student ID : " + this.studentId);
+        IO.println("Student Name : " + this.studentName);
+        IO.println("GPA : " + this.gpa);
+    }
+
+    public String getInfo(){
+        return this.studentId + " " + this.studentName + " " +this.gpa;
+    }
+}
